e99206cdd5273aa1a4bf536bbf8a8f17f6053d95
Eugene Shilow <jvshilov@gmail.com>
2024-11-06 15:52:30 +0500

    last commit before copying from repo

--- train_gpt2.py
+++ train_gpt2.py
@@ -94,13 +94,16 @@ class GPT(nn.Module):
 
         # init params
         self.apply(self._init_weights)
+        
+        # weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
 
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
             std = 0.02
             if hasattr(module, 'NANOGPT_SCALE_INIT'):
                 std *= (2 * self.config.n_layer) ** -0.5
-            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
             if module.bias is not None:
                 torch.nn.init.zeros_(module.bias)
         elif isinstance(module, nn.Embedding):
@@ -200,35 +203,55 @@ class GPT(nn.Module):
         optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
         return optimizer
         
+import tiktoken 
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (self.B * self.T)} batches")
+        
+        self.current_position = 0 
+    
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position:self.current_position + B*T + 1]
+        x = buf[:-1].view(B, T)
+        y = buf[1:].view(B, T)
+        self.current_position += B*T
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_position = 0
+        return x, y
+
+
+
 # -----------------------------------------------------------------------------
-
-num_return_sequences = 5
-max_length = 30
-
-# model = GPT.from_pretrained("gpt2")
-model = GPT(GPTConfig())
-model.eval()
+# select the compute device
 device = "mps" if torch.backends.mps.is_available() else "cpu"
+
+train_loader = DataLoaderLite(B=4, T=32)
+
+# get logits
+model = GPT(GPTConfig())
 model.to(device)
+# logits, loss = model(x, y)
 
-import tiktoken
-enc = tiktoken.get_encoding("gpt2")
-tokens = enc.encode("Hello, I'm a language model")
-tokens = torch.tensor(tokens, dtype=torch.long)
-tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
-x = tokens.to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(10):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"loss: {loss.item()}")
 
-torch.manual_seed(42)
-while x.size(1) < max_length:
-    with torch.no_grad():
-        logits, _ = model(x)  # Unpack the tuple
-        logits = logits[:, -1, :]
-        probs = F.softmax(logits, dim=-1)
-        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
-        x_next = torch.multinomial(probs, num_samples=1)
-        x = torch.cat((x, x_next), dim=1)
-
-for i in range(num_return_sequences):
-    tokens = x[i, :max_length].tolist()
-    decoded = enc.decode(tokens)
-    print(">", decoded)
+print(loss)
+import sys; sys.exit(0)
